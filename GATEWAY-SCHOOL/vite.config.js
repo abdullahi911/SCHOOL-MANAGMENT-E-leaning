cd509a3df13bc8081b1ba48f509fc364e5c73252
Abdullahi Abdikarin Abdullahi <apdilaahiapdi143@gmail.com>
2026-04-06 23:09:04 +0300

    fixin

--- GATEWAY-SCHOOL/vite.config.js
+++ GATEWAY-SCHOOL/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.BASE_URL || '/SCHOOL-MANAGMENT-E-leaning',
+  base: import.meta.env.VITE_BASE_URL || '/SCHOOL-MANAGMENT-E-leaning/',
   plugins: [
     react(),
     tailwindcss(),
